2493b38a4faa2c51a6f610caae3aae531913763d
anandavallis <sanandavalli7@gmail.com>
2025-05-12 09:28:06 +0530

    Create object-1.js

---
+++ object-1.js
@@ -0,0 +1,18 @@
+ <h2>Creating and Updating Object</h2>
+    
+    let person = { 
+        name : "Anandavalli", 
+        age : 22,
+        city : "Bangalore"
+    };
+
+    console.log("Name :" ,person.name);
+    console.log("Age :" , person.age);
+    console.log("City :" , person.city);
+    
+    // Update One Porperty
+    person.city = "NewYork";
+
+    console.log("Updated person object :" , person);
+
+
