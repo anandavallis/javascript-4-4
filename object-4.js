e0f8733ba5c875752be262721412d5adb15e692d
anandavallis <sanandavalli7@gmail.com>
2025-05-12 09:30:02 +0530

    Create object-4.js

---
+++ object-4.js
@@ -0,0 +1,16 @@
+
+    const users = [
+        { "name": "Alice", 
+        "age": 28, 
+        "city": "New York" },
+
+        { "name": "Bob", 
+        "age": 35, 
+        "city": "Los Angeles" },
+
+        { "name": "Charlie", 
+        "age": 22,
+         "city": "Chicago" }
+    ];
+
+    console.log(users);
